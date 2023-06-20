3971721bb791ceb94cde5481082323a876ee0aba
AlAzmine482 <137209260+AlAzmine482@users.noreply.github.com>
2023-06-20 11:35:46 -0700

    Update proj2.py

--- proj2.py
+++ proj2.py
@@ -1,5 +1,9 @@
 #proj2.py
-
+#python3 proj2.py Baselight_JJacobs_20230323.txt Flame_DFlowers_20230323.txt Flame_MFelix_20230323.txt -xytech Xytech_20230323.txt --output db
+#python3 proj2.py Baselight_TDanza_20230324.txt -xytech Xytech_20230324.txt --output db
+#python3 proj2.py Baselight_GLopez_20230325.txt -xytech Xytech_20230325.txt --output db
+#python3 proj2.py Baselight_THolland_20230326.txt Flame_DFlowers_20230326.txt -xytech Xytech_20230326.txt --output db
+#python3 proj2.py Baselight_THolland_20230327.txt Flame_DFlowers_20230327.txt -xytech Xytech_20230327.txt --output db
 import argparse
 import collections
 import csv
